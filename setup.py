df7e7b7a220dcfefc4d31fc1f01fb1518023c425
Ryan P Kilby <rpkilby@ncsu.edu>
2016-06-13 12:07:27 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -21,7 +21,7 @@ def test_suite():
 
 setup(
     name='SurveyGizmo',
-    version='1.2.0',
+    version='1.2.1',
     description='A Python Wrapper for SurveyGizmo\'s restful API service.',
     long_description=README,
     author='Ryan P Kilby',
